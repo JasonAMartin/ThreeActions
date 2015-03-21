3299e56a037afac9b2ed6532eac853ce088a087b
Jason A. Martin <jason.martin@jasonamartin.com>
2015-03-20 20:22:02 -0700

    Single Action Viewing
    The single action VC now changes BG color and displays the right action data that was chosen. It also has a back button and a complete action button (lacking complete code).

--- ThreeActions/SingleActionViewController.swift
+++ ThreeActions/SingleActionViewController.swift
@@ -17,16 +17,108 @@ class SingleActionViewController: UIViewController {
     var actionStatus = 0
     var actionDate = ""
     
+    //make elements
+    
+    let exitButton:UIButton = UIButton(frame: CGRectMake(100, 400, 100, 50))
+    var actionTitleLabel = UILabel(frame: CGRectMake(0, 0, 200, 21))
+    var actionDescriptionLabel = UILabel(frame: CGRectMake(0, 0, 200, 21))
+    var actionDateLabel = UILabel(frame: CGRectMake(0, 0, 200, 21))
+    let completeButton:UIButton = UIButton(frame: CGRectMake(100, 400, 100, 50))
+    
+
+    
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        self.view.backgroundColor = UIColor.appErrorRed()
+
+        //set BG color
+        if(actionColor==1) {
+            view.backgroundColor = UIColor.appActionOne()
+        }
         
-        println(actionDate)
-        println(actionStatus)
-        println(actionTitle)
-        println(actionDescription)
-        println(actionColor)
+        if(actionColor==2) {
+            view.backgroundColor = UIColor.appActionTwo()
+        }
+        
+        if(actionColor==3) {
+            view.backgroundColor = UIColor.appActionThree()
+        }
+        
+        
+        exitButton.setTitle("GO BACK", forState: .Normal)
+        exitButton.titleLabel!.font =  UIFont(name: "HelveticaNeue", size: 18)
+        exitButton.addTarget(self, action: "hideDisplay:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
+        
+        actionTitleLabel.text = actionTitle
+        actionTitleLabel.textAlignment = .Center
+        actionTitleLabel.textColor = UIColor.appMidnight()
+        actionTitleLabel.numberOfLines = 3
+        
+
+        actionDateLabel.text = actionDate
+        actionDateLabel.textAlignment = .Center
+        actionDateLabel.textColor = UIColor.appMidnight()
+        actionDateLabel.numberOfLines = 1
+        
+        actionDescriptionLabel.text = actionDescription
+        actionDescriptionLabel.textAlignment = .Center
+        actionDescriptionLabel.textColor = UIColor.appMidnight()
+        actionDescriptionLabel.numberOfLines = 6
+        
+        completeButton.setTitle("MARK AS COMPLETED", forState: UIControlState.Normal)
+        completeButton.hidden = false
+        UIViewController.actionSectionButton(completeButton)
+
+        if(actionStatus != 0) {
+            completeButton.hidden = true
+        }
+        
+        
+        self.view.addSubview(exitButton)
+        self.view.addSubview(actionDescriptionLabel)
+        self.view.addSubview(actionTitleLabel)
+        self.view.addSubview(actionDateLabel)
+        self.view.addSubview(completeButton)
+        
+        
+        //layout
+        
+        layout(exitButton, view) {exitButton, view in
+            exitButton.bottom == view.bottom - 20
+            exitButton.width == view.width
+        }
+        
+        layout(completeButton, view) {completeButton, view in
+            completeButton.bottom == view.bottom - 120
+            completeButton.width == 200
+            completeButton.height == 40
+        }
+        
+        
+        
+        layout(actionTitleLabel, view) {actionTitleLabel, view in
+            actionTitleLabel.top == view.top + 20
+            actionTitleLabel.width == view.width
+            actionTitleLabel.centerX == view.centerX
+        }
+        
+        
+        layout(actionDescriptionLabel, view) {actionDescriptionLabel, view in
+            actionDescriptionLabel.top == view.top + 80
+            actionDescriptionLabel.width == view.width
+            actionDescriptionLabel.centerX == view.centerX
+        }
+        
+        
+        layout(actionDateLabel, view) {actionDateLabel, view in
+            actionDateLabel.top == view.top + 200
+            actionDateLabel.width == view.width
+            actionDateLabel.centerX == view.centerX
+        }
+        
+       
         
     }
     
@@ -41,6 +133,10 @@ class SingleActionViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func hideDisplay(sender:AnyObject){
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     
